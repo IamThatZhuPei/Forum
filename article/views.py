fb9e01e79c6bdf4bbdd5ec1db29581de7b11ebcf
zhu pei <zp2332@qq.com>
2016-10-17 23:43:46 +0800

    learn 1017

--- article/views.py
+++ article/views.py
@@ -2,15 +2,33 @@
 from django.shortcuts import render
 from block.models import Block
 from .models import Article
+from django.core.paginator import Paginator
 
 __author__ = '朱沛'
 
+ARTICLE_CNT_1PAGE = 3
+
 
 def article_list(request, block_id):
+
+    page_no = int(request.GET.get("page_no", "1"))
+
     block_id = int(block_id)
     # print(block_id)
     block = Block.objects.get(id=block_id)
-    article_objs = Article.objects.filter(block=block, status=0).order_by("-id")
+    all_article_objs = Article.objects.filter(block=block, status=0).order_by("-id")
+    p = Paginator(all_article_objs, ARTICLE_CNT_1PAGE)
+    page = p.page(page_no)
     # article_objs = Article.objects.filter(block__id=block_id, status=0).order_by("-id")
 
-    return render(request, "article_list.html", {"articles": article_objs, "b": block})
+    page_elem = dict()
+    page_elem["page_cnt"] = p.num_pages
+    page_elem["current_no"] = page_no
+    page_elem["page_links"] = [i for i in range(page_no - 5, page_no + 6) if 0 < i <= p.num_pages]
+    page_elem["previous_link"] = page_elem["page_links"][0] - 1
+    page_elem["next_link"] = page_elem["page_links"][-1] + 1
+    page_elem["has_previous"] = page_elem["previous_link"] > 0
+    page_elem["has_next"] = page_elem["next_link"] <= page_elem["page_cnt"]
+
+
+    return render(request, "article_list.html", {"articles": page, "b": block, "page_elem": page_elem})
